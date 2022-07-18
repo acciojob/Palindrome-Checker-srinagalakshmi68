c565fae4a60c7c0bcafbf00406099a3589c45468
Srinagalakshmi <srinagalakshmi68@.com>
2022-07-18 20:40:18 +0530

    added

--- solution.js
+++ solution.js
@@ -1,6 +1,27 @@
 // complete the given function
 
-function palindrome(str){
+function palindrome(str) {
+    var len = str.length;
+
+    // Traversing through the string
+    // upto half its length
+    for (var i = 0; i < len / 2; i++) {
+
+        // Comparing ith character
+        // from starting and len-ith
+        // character from end
+        if (str[i] != str[len - i - 1])
+            return false;
+    }
+
+    // If the above loop doesn't return then it is
+    // palindrome
+    return true;
 
 }
-module.exports = palindrome
+//module.exports = palindrome
+let st = prompt();
+if (checkPalindrome(st) == true)
+    document.write("Yes");
+else
+    document.write("No");
